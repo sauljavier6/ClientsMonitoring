929dba5e0f6069660fd7b16982cc403933983780
Saul Javier Peralta Gomez <125310857+sauljavier6@users.noreply.github.com>
2025-07-28 11:31:12 -0700

    correccion en app rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes.js';
+// App.tsx
+import { BrowserRouter as Router } from 'react-router-dom'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
-
   return (
-      <Router>
-        <AppRoutes />
-      </Router>
+    <Router basename="/portalVisitas">
+      <AppRoutes />
+    </Router>
   )
 }
 
